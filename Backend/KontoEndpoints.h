178d2d32dfbf97dbbe4237b2323880f1fc4411fc
Guilian Newiger <guilian.newiger@yahoo.com>
2024-10-18 11:02:59 +0200

    dont need extra backend layer

--- Backend/KontoEndpoints.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Page.h"
-
-namespace Konto {
-	const int numberOfEndpoints = 1;
-	extern const Page::Page pages[numberOfEndpoints];
-}
--- Backend/Router.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-namespace Router {
-	const Response::Response route(const char* url, const char* method);
-}
